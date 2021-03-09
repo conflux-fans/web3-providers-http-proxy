edd6bc90bb9fb8b3c08b7bb75ad3c524f6df77d5
PanaW <wangpan@conflux-chain.org>
2021-03-09 16:41:53 +0800

    use local account support sendTransaction

--- src/ethToCfxAdaptors/eth_sendTransaction.js
+++ src/ethToCfxAdaptors/eth_sendTransaction.js
@@ -1,5 +1,7 @@
 const Adaptor = require('../JsonRPCAdaptor');
 const format = require('../format');
+const util = require('../util');
+const { Transaction } = require('js-conflux-sdk');
 
 /**
  * eth method: eth_sendTransaction
@@ -11,10 +13,21 @@ const format = require('../format');
  * 
  */
 
+function methodAdaptor(method, params) {
+  let hexAddress = format.formatHexAddress(params[0].from);
+  let address = format.formatAddress(hexAddress, this.cfx.networkId);
+  return this.cfx.wallet.has(address) ? 'cfx_sendRawTransaction' : method;
+}
+
 async function inputAdaptor(params) {
-  if (params.length > 0) {
-    params[0] = await format.formatTxParams(this.cfx, params[0]);
+  if (params.length === 0) throw new Error('The first parameter should be an transaction');
+  params[0] = await format.formatTxParams(this.cfx, params[0]);
+  if (this.cfx.wallet.has(params[0].from)) {
+    let tx = new Transaction(params[0]);
+    let account = this.cfx.wallet.get(params[0].from);
+    tx.sign(account.privateKey, this.cfx.networkId);
+    params[0] = tx.serialize();
   }
 }
 
-module.exports = new Adaptor(inputAdaptor);
+module.exports = new Adaptor(inputAdaptor, util.asyncEmptyFn, methodAdaptor);
--- src/index.js
+++ src/index.js
@@ -11,22 +11,46 @@ class Web3HttpProviderProxy extends Web3HttpProvider {
     if (!options.networkId) {
       throw new Error("options.networkId is needed");
     }
-    this.cfx = new Conflux({
+    let cfx = new Conflux({
       url: host,
       networkId: options.networkId
     });
+
+    //
+    let addresses = [];
+    if (options.privateKeys && Array.isArray(options.privateKeys)) {
+      for(let key of options.privateKeys) {
+        let account = cfx.wallet.addPrivateKey(key);
+        addresses.push(account.address);
+      }
+    }
+    if (options.mnemonic) {
+
+    }
+    this.addresses = addresses;
+    this.cfx = cfx;
   }
 
   send(payload, callback) {
     let originMethod = payload.method;
     let adaptor = ethToConflux[originMethod] || defaultAdaptor;
     adaptor.cfx = this.cfx;
+
+    // hack eth_accounts method
+    if (this.addresses.length > 0 && payload.method === 'eth_accounts') {
+      callback(null, {
+        result: this.addresses.map((a) => format.formatHexAddress(a)),
+        jsonrpc: '2.0',
+        id: payload.id
+      });
+      return;
+    }
     
     const promiseSend = () => new Promise((resolve, reject) => {
       debug(`Proxy ${originMethod} to ${payload.method}`);
       debug(`Proxy params: ${JSON.stringify(payload, null, '\t')}`);
       super.send(payload, (err, response) => {
-        // console.log(err, response);
+        console.log(err, response);
         err ? reject(err) : resolve(response)
       });
     });

6a9225ab2592cfc5fb426fc949d2478fb30bb330
xiongyu <xlovebear@126.com>
2021-03-18 18:54:48 +0800

    fix bug

--- test/JsonRPCEngine.js
+++ test/JsonRPCEngine.js
@@ -1,58 +1,54 @@
-const { JsonRpcEngine, createAsyncMiddleware, createScaffoldMiddleware}= require('json-rpc-engine');
-
-const format = require('../src/format');
+const { JsonRpcEngine }= require('json-rpc-engine');
+const { util } = require('../src/');
 const pify = require('pify');
+const axios = require('axios');
 
 const engine = new JsonRpcEngine();
 const { Conflux } = require('js-conflux-sdk');
 
+const { HttpProvider, ethToConflux, util: {buildJsonRpcRequest}} = require('../src');
+const createProxyMiddleware = require('../src/jsonRpcEngine/inputAdaptor');
+//const ProxyMiddleware = require('../src/proxy');
+
 const URL = 'https://testnet-rpc.conflux-chain.org.cn/v2';
-const conflux = new Conflux({
-    url: URL,
+
+const provider = new HttpProvider(URL, {
+    chainAdaptor: ethToConflux,
     networkId: 1,
+    privateKeys: [
+      '0x3f841bf589fdf83a521e55d51afddc34fa65351161eead24f064855fc29c9580',
+    ]
 });
 
 const address0 = 'cfxtest:aanu79h2zdyhamsfc5jaea5r0b2pdh5jky362gbbhn';
 const address0_Hex = '0x170efcf8a8e87029c516d002036db070c19f684d';
 
-async function inputAdaptor(params, networkId) {
-    params[0] = format.formatAddress(params[0], networkId);
-    format.formatEpochOfParams(params, 1);
-    return params;
-} 
-
-// get address balance
-async function getBalance (req, res) {
-    console.log(req);
-    console.log(req.params[0]);
-    //res.result = Number(await conflux.getBalance(req.params)) / 1e18;
-    if(req.params) {
-        req.params = inputAdaptor(req.params, 1);
-    }
-    res = req;
-    return res; 
-}
-
-function createMiddleware() {
-    return createScaffoldMiddleware({
-        'eth_getBalance': createAsyncMiddleware(getBalance),
-    });
-}
-
-function buildJsonRpcRequest(method, ...params) {
-    return {
-      "jsonrpc": "2.0",
-      "id": Date.now().toString(),
-      method,
-      params,
-    }
-}
-
 async function testEngine() {
-    engine.push(createMiddleware());
-    let payload = buildJsonRpcRequest('eth_getBalance', address0_Hex);
-    let getterResponse = await pify(engine.handle).call(engine, payload);
-    console.log(getterResponse);
+    /*
+    let payload = {
+        jsonrpc: '2.0',
+        id: '1616062397449',
+        method: 'cfx_getBalance',
+        params: [ 'cfxtest:aanu79h2zdyhamsfc5jaea5r0b2pdh5jky362gbbhn' ]
+    }
+    let res = await axios.post(URL, payload);
+    console.log(res.data);
+    return;
+    */
+    engine.push(createProxyMiddleware(provider, URL));
+    
+    let payload_0 = buildJsonRpcRequest('eth_getBalance', address0_Hex);
+    let res_0 = await pify(engine.handle).call(engine, payload_0);
+    console.log(res_0);
+    return;
+    let data = {
+        from: '0x13d2ba4ed43542e7c54fbb6c5fccb9f269c1f94c',
+        to: '0x1386b4185a223ef49592233b69291bbe5a80c527',
+        value: util.numToHex(100)
+      };
+    let payload_1 = buildJsonRpcRequest('eth_estimateGas', data);
+    let res_1 = await pify(engine.handle).call(engine, payload_1);
+    console.log(res_1);
 }
 
 testEngine();
